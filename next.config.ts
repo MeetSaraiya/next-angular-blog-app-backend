fdc3c301a215a5de3652769f0fe42501f9a78b8a
Meet Saraiya <127649875+MeetSaraiya@users.noreply.github.com>
2025-03-10 17:28:02 +0530

    changes2

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         headers:[
           {key:'Access-Control-Allow-Credentials', value:'true'},
           {key:'Access-Control-Allow-Origin', value:'http://localhost:4200'},
-          {key:'Access-Control-Allow-Origin', value:'https://next-angular-blog-app-backend.vercel.app'},{key:'Access-Control-Allow-Methods', value:'GET, POST, PUT, DELETE, OPTIONS , PATCH'},
+          {key:'Access-Control-Allow-Origin', value:'https://next-angular-blog-app-frontend.vercel.app'},{key:'Access-Control-Allow-Methods', value:'GET, POST, PUT, DELETE, OPTIONS , PATCH'},
           {key:'Access-Control-Allow-Headers', value:'X-CSRF-Token, X-Requested-With, Content-Type, Accept, Authorization,Accept,Accept-version,Content-Length,Content-Type,Date,X-Api-Version,Content-MD5'},
           
         ]
