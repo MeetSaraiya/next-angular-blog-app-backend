a9673724cb6d00bbc5c00c8f7cf97fa11deab8d9
Meet Saraiya <127649875+MeetSaraiya@users.noreply.github.com>
2025-03-10 17:24:56 +0530

    changes-cors

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,8 @@ const nextConfig: NextConfig = {
         source:"/api/:path*",
         headers:[
           {key:'Access-Control-Allow-Credentials', value:'true'},
-          {key:'Access-Control-Allow-Origin', value:'http://localhost:4200'},{key:'Access-Control-Allow-Methods', value:'GET, POST, PUT, DELETE, OPTIONS , PATCH'},
+          {key:'Access-Control-Allow-Origin', value:'http://localhost:4200'},
+          {key:'Access-Control-Allow-Origin', value:'https://next-angular-blog-app-backend.vercel.app'},{key:'Access-Control-Allow-Methods', value:'GET, POST, PUT, DELETE, OPTIONS , PATCH'},
           {key:'Access-Control-Allow-Headers', value:'X-CSRF-Token, X-Requested-With, Content-Type, Accept, Authorization,Accept,Accept-version,Content-Length,Content-Type,Date,X-Api-Version,Content-MD5'},
           
         ]
